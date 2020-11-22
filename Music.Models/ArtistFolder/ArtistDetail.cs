3dcb1cc042f459c2962a70bf5a3b0a25739476ec
JimGulley40 <jimgulley40@gmail.com>
2020-11-22 10:19:10 -0500

    added artist and full crud controller

---
+++ Music.Models/ArtistFolder/ArtistDetail.cs
@@ -0,0 +1,20 @@
+﻿using Music.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music.Models
+{
+    public class ArtistDetail
+    {
+        [Key]
+        public int ArtistId { get; set; }
+        public Guid OwnerId { get; set; }
+        public object ArtistName { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+        public object ModifiedUtc { get; set; }
+    }
+}
