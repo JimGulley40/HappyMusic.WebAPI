1f871bab1fbc9e35d792955c3fb93b340258661f
JimGulley40 <72030090+JimGulley40@users.noreply.github.com>
2020-11-24 14:36:30 -0500

    added swagger and XMLComments

--- HappyMusic.WebAPI/Controllers/SongController.cs
+++ HappyMusic.WebAPI/Controllers/SongController.cs
@@ -12,19 +12,31 @@ namespace HappyMusic.WebAPI.Controllers
 {
     public class SongController : ApiController
     {
+        /// <summary>
+        /// Gets a list of all songs
+        /// </summary>
+        /// <returns></returns>
         public IHttpActionResult Get()
         {
             SongService songService = CreateSongService();
             var songs = songService.GetSongs();
             return Ok(songs);
         }
-
+        /// <summary>
+        /// creates a song
+        /// </summary>
+        /// <returns></returns>
         private SongService CreateSongService()
         {
             
             var songService = new SongService();
             return songService;
         }
+        /// <summary>
+        /// posts a song
+        /// </summary>
+        /// <param name="song">pass a song name</param>
+        /// <returns></returns>
         public IHttpActionResult Post(SongCreate song)
         {
             if (!ModelState.IsValid)
@@ -37,13 +49,22 @@ namespace HappyMusic.WebAPI.Controllers
 
             return Ok();
         }
-
+        /// <summary>
+        /// get a song by ID
+        /// </summary>
+        /// <param name="id">Pass a song ID</param>
+        /// <returns></returns>
         public IHttpActionResult Get(int id)
         {
             SongService songService = CreateSongService();
             var song = songService.GetSongById(id);
             return Ok(song);
         }
+        /// <summary>
+        /// Edit a specific song
+        /// </summary>
+        /// <param name="song">Pass a song name</param>
+        /// <returns></returns>
         public IHttpActionResult Put(SongEdit song)
         {
             if (!ModelState.IsValid)
@@ -56,13 +77,17 @@ namespace HappyMusic.WebAPI.Controllers
 
             return Ok();
         }
+        /// <summary>
+        /// Delete a song by ID
+        /// </summary>
+        /// <param name="id">Pass a song ID</param>
+        /// <returns></returns>
         public IHttpActionResult Delete(int id)
         {
             var service = CreateSongService();
 
             if (!service.DeleteSong(id))
                 return InternalServerError();
-
             return Ok();
         }
 
